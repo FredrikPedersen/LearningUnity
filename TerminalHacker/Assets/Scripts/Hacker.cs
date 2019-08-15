fdfb86eeb1bab91f3e6eb49fb8ff5fa98ca4ed71
FredrikPedersen <fredrikhp@gmail.com>
2019-08-15 21:47:24 +0200

    Completed Section 2: Part 27

--- TerminalHacker/Assets/Scripts/Hacker.cs
+++ TerminalHacker/Assets/Scripts/Hacker.cs
@@ -1,10 +1,15 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour {
 
+    //Game configuration data
+    string[] level1Passwords = { "hei", "hade", "kake", "bok", "dør" };
+    string[] level2Passwords = { "panteflasker", "kebab", "orakel", "ostepop", "mordi" };
+    string[] level3Passwords = { "skosnipp", "eplegnuffel", "onomatopoetikon", "kebabsaus", "panteflasker" };
+
+    //Game State
     int level;
-    String message = "";
+    string password;
     enum Screen {MainMenu, Password, Win};
     Screen currentScreen;
 
@@ -16,11 +21,6 @@ public class Hacker : MonoBehaviour {
     void ShowMainMenu() {
         currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
-
-        if (message != "") {
-            Terminal.WriteLine(message);
-        }
-
         Terminal.WriteLine("Hva ønsker du å hacke? \n 1. Biblioteket i P35 \n 2. Kontoret til Sigrun \n 3. Oraklenes topp-hemmelige data ");
         Terminal.WriteLine("\nSkriv inn ditt valg (1, 2 eller 3): ");
     }
@@ -31,21 +31,15 @@ public class Hacker : MonoBehaviour {
         } else if (currentScreen == Screen.MainMenu) {
             RunMainMenu(input);
         } else if (currentScreen == Screen.Password) {
-            RunPassword();
+            CheckPassword(input);
         }
     }
 
-    private void RunPassword() {
-        message = "FEIL PASSORD!";
-        ShowMainMenu();
-    }
-
     void RunMainMenu(string input) {
-        if (input == "1") {
-            level = 1;
-            startGame();
-        } else if (input == "2") {
-            level = 2;
+
+        bool isValidLevelNumber = (input == "1" || input == "2" || input == "3");
+        if (isValidLevelNumber) {
+            level = int.Parse(input);
             startGame();
         } else if (input.ToLower() == "hello there") {
             Terminal.WriteLine("GENERAL KENOBI");
@@ -56,6 +50,54 @@ public class Hacker : MonoBehaviour {
 
     void startGame() {
         currentScreen = Screen.Password;
+        Terminal.ClearScreen();
+
+        switch (level) {
+            case 1:
+                password = level1Passwords[Random.Range(0, level1Passwords.Length)];
+                break;
+            case 2:
+                password = level2Passwords[Random.Range(0, level2Passwords.Length)];
+                break;
+            case 3:
+                password = level3Passwords[Random.Range(0, level3Passwords.Length)];
+                break;
+            default:
+                Debug.LogError("Invalid level number!");
+                break;
+        }
+
         Terminal.WriteLine("Vennligst oppgi ditt passord: ");
+    }
+
+    void CheckPassword(string input) {
+        if (input == password) {
+            DisplayWinScreen();
+        } else {
+            Terminal.WriteLine("FEIL PASSORD!");
+        }
+    }
+
+    void DisplayWinScreen() {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        ShowLevelReward();
+    }
+
+    void ShowLevelReward() {
+        switch (level) {
+            case 1:
+                Terminal.WriteLine("Du hacker biblioteket og har nå evig print credits!");
+                break;
+            case 2:
+                Terminal.WriteLine("Du dobler lønna i \narbeidskontrakten din!");
+                break;
+            case 3:
+                Terminal.WriteLine("Du innser at Oraklenes store \nhemmelighet er Google og bortgjemte \nsoveposer på datatorget.");
+                Terminal.WriteLine("Ingen snarveier til kodehimmelen :(");
+                break;
+            default:
+                break;
+        }
     }
 }
