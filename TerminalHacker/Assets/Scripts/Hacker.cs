d705a246efa43974cecb4e035e133fa150a97c73
FredrikPedersen <fredrikhp@gmail.com>
2019-08-15 20:01:45 +0200

    Finnished Section 2: Part 22.

--- TerminalHacker/Assets/Scripts/Hacker.cs
+++ TerminalHacker/Assets/Scripts/Hacker.cs
@@ -1,25 +1,42 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Hacker : MonoBehaviour {
+
+    int level;
+    enum Screen {MainMenu, Password, Win};
+    Screen currentScreen;
+
     // Start is called before the first frame update
     void Start() {
         ShowMainMenu();
     }
 
     void ShowMainMenu() {
-        var username = "sup3rh4ckerxX1337Xx";
-        var greeting = "God dag " + username;
+        currentScreen = Screen.MainMenu;
         Terminal.ClearScreen();
 
-        Terminal.WriteLine(greeting);
-        Terminal.WriteLine("Hva ønsker du å hacke? \n 1. Biblioteket i P35 \n 2. Kontoret til Sigrun \n 3. Oraklenes super topp-hemmelige data ");
-        Terminal.WriteLine("Skriv inn ditt valg: ");
+        Terminal.WriteLine("Hva ønsker du å hacke? \n 1. Biblioteket i P35 \n 2. Kontoret til Sigrun \n 3. Oraklenes topp-hemmelige data ");
+        Terminal.WriteLine("Skriv inn ditt valg (1, 2 eller 3): ");
     }
 
-    // Update is called once per frame
-    void Update() {
-        
+    void OnUserInput(string input) {
+        if (input.ToLower() == "meny") {
+            ShowMainMenu();
+        } else if (input == "1") {
+            level = 1;
+            startGame();
+        } else if (input == "2") {
+            level = 2;
+            startGame();
+        } else if (input.ToLower() == "hello there") {
+            Terminal.WriteLine("GENERAL KENOBI");
+        } else {
+            Terminal.WriteLine("Vennligst oppgi et gyldig hackermål");
+        }
+     }
+
+    void startGame() {
+        currentScreen = Screen.Password;
+        Terminal.WriteLine("Du har valgt alternativ " + level);
     }
 }
