8379ca3254fcd71ef85efc1463330314b18fbe55
Fredrik Pedersen <fredrikhp@gmail.com>
2019-08-15 16:49:55 +0200

    Continued working on Hacker.cs

---
+++ TerminalHacker/Assets/Scripts/Hacker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        ShowMainMenu();
+    }
+
+    void ShowMainMenu() {
+        var username = "sup3rh4ckerxX1337Xx";
+        var greeting = "God dag " + username;
+        Terminal.ClearScreen();
+
+        Terminal.WriteLine(greeting);
+        Terminal.WriteLine("Hva ønsker du å hacke? \n 1. Biblioteket i P35 \n 2. Kontoret til Sigrun \n 3. Oraklenes super topp-hemmelige data ");
+        Terminal.WriteLine("Skriv inn ditt valg: ");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        
+    }
+}
